86327738eb489b01b9e0e1cd855701c4b4a6de45
Treinamento <Treinamento@RD723897.raiadrogasil.com>
2020-03-06 12:40:01 -0300

    adicionando click na nav para destacar

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,4 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { isNgTemplate } from '@angular/compiler';
+
+interface iItem {
+  label: string;
+  route: string;
+  ativo: boolean;
+}
+
 
 @Component({
   selector: 'app-nav',
@@ -7,7 +15,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
-  constructor() { }
+
+
+  itens: iItem[] = [
+    {ativo: true,
+    label: "Home",
+    route: "/"
+    },
+
+    {
+      ativo: false,
+      label:"Sobre",
+      route:"/sobre"
+
+    },
+    {
+      ativo:false,
+      label:"Contato",
+      route: "/contato"
+    }
+  ]
+
+  constructor() { 
+
+  }
+
+//fazendo com que todos os itens fiquem inativos
+
+  destacar(item:iItem){
+    this.itens.forEach(
+    (item) => 
+      {item.ativo = false}
+    )
+
+//ativo o item
+
+  item.ativo = true;
+    }
 
   ngOnInit(): void {
   }
